c7ab27c73e7589031ae29e8b88f2dfa8e6157666
Dinesh <dinesh@vasa.com>
2025-07-05 16:07:00 +0530

    variable is started

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,9 +1,10 @@
  #!/bin/bash
 
+NUMBER=$1
+
  if [ $ Number -lt 10 ]
  then
- echo "Given number $NUMBER is less than 10"
+    echo "Given number $NUMBER is less than 10"
  else
- echo "Given number $NUMBER is not less than 10"
+    echo "Given number $NUMBER is not less than 10"
  fi
- 
