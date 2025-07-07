98558ef61745c5d75201ba9b81ea91c1185b29dc
Dinesh <dinesh@vasa.com>
2025-07-07 19:04:18 +0530

    colors

--- 16-color.sh
+++ 16-color.sh
@@ -3,9 +3,9 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-echo -e "${GREEN}INFO: Process completed successfully.${NC}"
-echo -e "${YELLOW}WARNING: Low disk space.${NC}"
-echo -e "${RED}ERROR: File not found.${NC}"
+echo -e "$GREEN INFO: Process completed successfully $NC"
+echo -e "$YELLOW WARNING: Low disk space $NC"
+echo -e "$RED ERROR: File not found $NC"
 
 # Output:
 # INFO: Process completed successfully. (in green color)
