c0acfbfbace97f40c9819b305f27c52f385d6156
Dinesh <dinesh@vasa.com>
2025-07-07 19:02:05 +0530

    colors

---
+++ 16-color.sh
@@ -0,0 +1,13 @@
+ RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+echo -e "${GREEN}INFO: Process completed successfully.${NC}"
+echo -e "${YELLOW}WARNING: Low disk space.${NC}"
+echo -e "${RED}ERROR: File not found.${NC}"
+
+# Output:
+# INFO: Process completed successfully. (in green color)
+# WARNING: Low disk space. (in yellow color)
+# ERROR: File not found. (in red color)
